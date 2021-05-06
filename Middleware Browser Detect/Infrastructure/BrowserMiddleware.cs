c7878ad944c061c07daf28edd732f36adb797271
Gabriel <camed.tf@gmail.com>
2021-05-06 13:22:48 +0200

    initial commit
    initial commit

---
+++ Middleware Browser Detect/Infrastructure/BrowserMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shyjus.BrowserDetection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware_Browser_Detect.Infrastructure
+{
+    public class BrowserMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public BrowserMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext, IBrowserDetector detector) 
+        {
+            var browser = detector.Browser;
+
+            if(browser.Name == BrowserNames.Edge || browser.Name == BrowserNames.EdgeChromium || browser.Name == BrowserNames.InternetExplorer)
+            {
+                await httpContext.Response.WriteAsync("Twoja przegladarka nie jest obslugiwana!");
+            }
+            else
+            {
+                await _next.Invoke(httpContext);
+            }
+        }
+    }
+}
---
+++ Middleware Browser Detect/Infrastructure/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware_Browser_Detect.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IApplicationBuilder UseBrowserMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<BrowserMiddleware>();
+        }
+    }
+}
